0a7b64519a0f550e3199fdf8382cd1587fe08e66
Fajar Sidiq Arrizal <fajarsidiq3000@gmail.com>
2020-08-31 21:06:27 +0700

    First Commit

---
+++ app/src/main/java/com/jar/fragment/ui/home/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.jar.fragment.ui.home;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.jar.fragment.R;
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup
+            container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home, null);
+    }
+}
---
+++ app/src/main/java/com/jar/fragment/ui/profile/ProfileFragments.java
@@ -0,0 +1,21 @@
+package com.jar.fragment.ui.profile;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.jar.fragment.R;
+
+public class ProfileFragments extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup
+            container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, null);
+    }
+}
+
